6d87a7672aee522f8fc8f665ddd1cf54823e71fd
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-15 16:25:56 +0330

    feat: improve header

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -12,55 +12,58 @@ export default function Header() {
   const [isOpen, setOpen] = useState(false);
 
   return (
-    <div className="relative md:inline-flex">
-      <div 
-        onClick={() => setOpen(!isOpen)}
-        className={clsx(
-          "md:hidden cursor-pointer transition-height transition-slowest ease fixed top-4 left-4 right-4 z-10 text-white bg-zinc-800 border-2 p-2 rounded-2xl",
-          { 'h-18 border-white text-white': !isOpen, 'border-orange-500 text-orange-500': isOpen },
-        )}>
-        <div className='flex items-center'>
-          <Hamburger size={20} toggled={isOpen} />
-          <div className='text-xl'>Menu</div>
-        </div>
-        {isOpen && header.map((link) => {
-          const isActive = link.slug === pathname
+    <>
+      <div className="relative md:inline-flex">
+        <div 
+          onClick={() => setOpen(!isOpen)}
+          className={clsx(
+            "md:hidden cursor-pointer transition-height transition-slowest ease fixed top-4", 
+            "left-4 right-4 z-10 text-white bg-zinc-800 border-2 p-2 rounded-2xl",
+            { 'h-18 border-white text-white': !isOpen, 'border-orange-500 text-orange-500': isOpen },
+          )}>
+          <div className='flex items-center'>
+            <Hamburger size={20} toggled={isOpen} />
+            <div className='text-xl'>Menu</div>
+          </div>
+          {isOpen && header.map((link) => {
+            const isActive = link.slug === pathname
 
-          return (!link?.isDisabled &&
-            (<div key={link.name} className={"p-2"}>
-              <Link
-                href={`/${link.slug}`}
-                className={clsx(
-                  "mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-zinc-500",
-                  { 'text-zinc-400': !isActive, 'text-orange-500': isActive },
-                )}
-                >
-                  {link.name}
-              </Link>
-            </div>)
-          );
-        })}
-      </div>
-      <div className="hidden md:flex">
-        {header.map((link) => {
-          const isActive = link.slug === pathname
-
-          return (!link?.isDisabled &&
-            (<div key={link.name}>
-              <Link
-                href={`/${link.slug}`}
-                className={clsx(
-                  "mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-zinc-500",
-                  { 'text-zinc-400': !isActive, 'text-orange-500': isActive },
-                )}
-                >
-                  {link.name}
+            return (!link?.isDisabled &&
+              (<div key={link.name} className={"p-2"}>
+                <Link
+                  href={`/${link.slug}`}
+                  className={clsx(
+                    "mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-orange-500",
+                    { 'text-zinc-500': !isActive },
+                  )}
+                  >
+                    {link.name}
                 </Link>
-            </div>)
-          );
-        })}
+              </div>)
+            );
+          })}
+        </div>
+        <div className="hidden md:flex">
+          {header.map((link) => {
+            const isActive = link.slug === pathname
+
+            return (!link?.isDisabled &&
+              (<div key={link.name}>
+                <Link
+                  href={`/${link.slug}`}
+                  className={clsx(
+                    "mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-orange-500",
+                    { 'text-zinc-500': !isActive },
+                  )}
+                  >
+                    {link.name}
+                  </Link>
+              </div>)
+            );
+          })}
+        </div>
       </div>
-    </div>
+    </>
   );
 
 }
