94836ea368e476168d7e0574167a30e22a972de0
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-15 16:10:51 +0330

    refactor: improve footer

--- src/ui/Footer.tsx
+++ src/ui/Footer.tsx
@@ -4,34 +4,40 @@ export default function Footer() {
   const d = new Date();
 
   return (
-    <div className="md:flex text-sm text-zinc-600">
-      {'©'} &nbsp; <span>{d.getFullYear()}</span> &nbsp; Mo Khashan. All Rights Reserved.
-      Created by the &nbsp;<b>Next.js</b>&nbsp; and 
-      &nbsp;
-      <Image
-        src="/butter-w.png"
-        unoptimized
-        alt="Butter CMS"
-        width={100}
-        height={0}
-      />
-      {'.'}
-      &nbsp;
-      <a
-        className="underline decoration-dotted underline-offset-4"
-        href="https://github.com/vercel/next.js/examples/with-turbopack"
-      >
-        View the code
-      </a>&nbsp;
-      {' or '}
-      &nbsp;
-      <a
-        className="underline decoration-dotted underline-offset-4"
-        href="https://vercel.com/templates/next.js"
-      >
-        deploy your own
-      </a>
-      {'.'}
+    <div className="justify-center md:inline-flex text-sm text-zinc-600">
+      <p className='flex-row md:flex'>
+        <span>{'©'} &nbsp; <span>{d.getFullYear()}</span> &nbsp; Mo Khashan. All Rights Reserved.</span>
+        <span className='flex md:block'>
+          Created by the &nbsp;<b>Next.js</b>&nbsp; and 
+          &nbsp;
+          <Image
+            src="/butter-w.png"
+            unoptimized
+            alt="Butter CMS"
+            width={100}
+            height={0}
+          />
+          {'.'}
+          &nbsp;
+        </span>
+      </p>
+      <p>
+        <a
+          className="underline decoration-dotted underline-offset-4"
+          href="https://github.com/vercel/next.js/examples/with-turbopack"
+        >
+          View the code
+        </a>&nbsp;
+        {' or '}
+        &nbsp;
+        <a
+          className="underline decoration-dotted underline-offset-4"
+          href="https://vercel.com/templates/next.js"
+        >
+          deploy your own
+        </a>
+        {'.'}
+      </p>
     </div>
   )
 }
