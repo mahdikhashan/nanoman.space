e8231cd1851f1bc5e9d2dba198b495087a929d5f
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-17 21:45:41 +0330

    refactor: reduce code

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -52,30 +52,15 @@ export default function Header() {
             return (!link?.isDisabled &&
               (<div key={link.name}>
                 {
-                !link?.redirect && 
                   <Link
                     href={`/${link.slug}`}
-                    className={clsx(
-                      "mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-orange-500",
-                      { 'text-zinc-500': !isActive },
-                    )}
-                    >
-                    {link.name}
-                  </Link>
-                }
-                {
-                link?.redirect && 
-                  <Link
-                    href={link.slug}
-                    passHref={true}
-                    target={"_blank"}
                     className={clsx(
                       "flex items-baseline gap-1 mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-orange-500",
                       { 'text-zinc-500': !isActive },
                     )}
-                  >
-                    {link.name}
-                    <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg>
+                    >
+                    {link?.name}
+                    {link?.redirect && <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg> }
                   </Link>
                 }
               </div>)
