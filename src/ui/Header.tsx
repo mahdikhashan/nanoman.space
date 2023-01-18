981b5be2d588673780a0017425a775b27f738cd3
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-18 23:36:22 +0330

    refactor: redundancy

--- src/ui/Header.tsx
+++ src/ui/Header.tsx
@@ -77,7 +77,7 @@ export default function Header() {
                 {
                   <Link
                     href={!link?.redirect ? `/${link.slug}` : link.slug}
-                    passHref={link?.redirect && true}
+                    passHref={link?.redirect}
                     target={link?.redirect ? "_blank" : "_self"}
                     className={clsx(
                       "flex items-baseline gap-1 mb-2 px-3 text-sm font-semibold uppercase tracking-wider text-orange-500",
@@ -85,7 +85,10 @@ export default function Header() {
                     )}
                     >
                     {link?.name}
-                    {link?.redirect && <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_nPIU"><path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path></svg> }
+                    {link?.redirect && 
+                    <svg width="13.5" height="13.5" aria-hidden="true" viewBox="0 0 24 24" className="iconExternalLink_nPIU">
+                      <path fill="currentColor" d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z"></path>
+                    </svg> }
                   </Link>
                 }
               </div>)
