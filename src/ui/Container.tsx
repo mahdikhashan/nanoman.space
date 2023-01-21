9cce1a8eb191d5ac2032f3fb6177e7f344a58b81
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-21 18:09:09 +0330

    style: improve bg color

--- src/ui/Container.tsx
+++ src/ui/Container.tsx
@@ -41,7 +41,7 @@ export default function Container(props: { children: any; customMeta?: any; }) {
 
       <main
         id="skip"
-        className="min-h-screen mx-auto overflow-y-auto dark:bg-zinc-800"
+        className="min-h-screen mx-auto overflow-y-auto bg-gray-100 dark:bg-zinc-800"
       >
         <div className="container mx-auto flex-grow">
           <div className="grid grid-cols-1 gap-y-8 p-4 md:py-8 md:px-12">
