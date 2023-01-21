f3a551b05b8ad6210fe0f0e9b2da75fffb2fe523
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-21 19:15:18 +0330

    chore: improve butter-cms typing

---
+++ src/lib/types.ts
@@ -0,0 +1,40 @@
+export type Post = {
+  status: string,
+  created: string,
+  updated: string,
+  published: string,
+  title: string,
+  slug: string,
+  body: string,
+  summary: string,
+  seo_title: string,
+  meta_description: string,
+  featured_image_alt: string,
+  url: string,
+  featured_image: unknown,
+  tags: Tag[],
+  categories: Category[],
+  author: Author
+}
+
+export type Category = {
+  name: string,
+  slug: string
+}
+
+export type Author = {
+  bio: string,
+  email: string,
+  slug: string,
+  title: string,
+  last_name: string,
+  first_name: string,
+  facebook_url: string,
+  linkedin_url: string,
+  instagram_url: string,
+  pinterest_url: string,
+  profile_image: string,
+  twitter_handle: string
+}
+
+export type Tag = {}
