2cb891fcb540eda03534f141be9271629d02e393
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-21 18:40:00 +0330

    style: improve bg color

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body className="dark:bg-zinc-800 bg-white">
+      <body className="dark:bg-zinc-800 bg-gray-100">
         <Main />
         <NextScript />
       </body>
