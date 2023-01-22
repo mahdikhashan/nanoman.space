019667c90d264b03dd837c044e8bd0ee985d9a50
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-22 15:44:43 +0330

    style: prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,29 +1,15 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css';
 
-import type { AppProps } from "next/app";
-import { useEffect, useState } from "react";
-import { ThemeProvider } from "next-themes";
-import { Analytics } from "@vercel/analytics/react";
+import { ThemeProvider } from 'next-themes';
+import { Analytics } from '@vercel/analytics/react';
+
+import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [showing, setShowing] = useState(false);
-
-  useEffect(() => {
-    setShowing(true);
-  }, []);
-
-  if (!showing) {
-    return null;
-  }
-
-  if (typeof window === "undefined") {
-    return <></>;
-  } else {
-    return (
-      <ThemeProvider enableSystem={true} attribute="class">
-        <Component {...pageProps} />
-        <Analytics />
-      </ThemeProvider>
-    );
-  }
+  return (
+    <ThemeProvider enableSystem={true} attribute="class">
+      <Component {...pageProps} />
+      <Analytics />
+    </ThemeProvider>
+  );
 }
