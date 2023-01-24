2efb79e6bc4787f38fd820b7da17808e5c5eb9d3
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-24 16:50:08 +0330

    chore: setup cypress

---
+++ cypress/e2e/app.cy.ts
@@ -0,0 +1,17 @@
+describe('Navigation', () => {
+  it('should navigate to the about page', () => {
+    // Start from the index page
+    cy.visit('http://localhost:3000/')
+
+    // Find a link with an href attribute containing "about" and click it
+    cy.get('a[href*="blog"]').click()
+
+    // The new url should include "/about"
+    cy.url().should('include', '/blog')
+
+    // The new page should contain an h1 with "All Blog Posts"
+    cy.get('h1').contains('All Blog Posts')
+  })
+})
+
+export {};
