c9210e9ac8cdfc253455a9fa5df94d1d70047656
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-26 00:02:12 +0330

    fix: style

--- src/ui/PostLink.tsx
+++ src/ui/PostLink.tsx
@@ -1,7 +1,7 @@
 import { Post } from '@/lib/types';
 import Link from 'next/link';
 
-type Props = Post;
+export type Props = Pick<Post, 'slug' | 'title' | 'categories'>;
 
 export default function PostLink({
   slug,
@@ -15,7 +15,7 @@ export default function PostLink({
         pathname: '/blog/[slug]',
         query: { slug }
       }}
-      className="flex flex-row py-4 justify-between hover:text-orange-500"
+      className="flex flex-row py-4 justify-between hover:text-orange-500 transition ease-in-out delay-50 hover:scale-[1.025]"
     >
       <div>{title}</div>
       <div className="text-zinc-500">
--- src/ui/ProjectLink.tsx
+++ src/ui/ProjectLink.tsx
@@ -8,7 +8,7 @@ export default function ProjectLink({ slug, name }: Props) {
     <Link
       href={`/`}
       key={slug}
-      className="block space-y-1.5 rounded-lg border border-black/10 dark:border-white/10 px-4 py-3 hover:border-black/20 dark:hover:border-white/20 dark:bg-zinc-800 bg-white"
+      className="block space-y-1.5 rounded-lg border border-black/10 dark:border-white/10 px-4 py-3 hover:border-black/20 dark:hover:border-white/20 dark:bg-zinc-800 bg-white transition ease-in-out delay-50 hover:scale-[1.025]"
     >
       <div>{name}</div>
       <div className="line-clamp-3 text-sm text-zinc-400">{slug}</div>
