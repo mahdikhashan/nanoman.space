649603e5110eb4a8a96ccbc28dacf5d42886f895
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-01-26 13:07:12 +0330

    style: enable prose

--- src/pages/blog/[slug].tsx
+++ src/pages/blog/[slug].tsx
@@ -1,28 +1,41 @@
-// @ts-nocheck
-
 import Container from '@/ui/Container';
-
-import { InferGetStaticPropsType } from 'next';
-import { useRouter } from 'next/router'
-
 import { getPost, getPostsData } from '@/lib/api';
+import type { Post } from '@/lib/types';
+import { format } from 'date-fns';
+import readingTime from 'reading-time';
 
-export default function Post({
-  post,
-}: InferGetStaticPropsType<typeof getStaticProps>) {
-  const router = useRouter()
-  const { slug } = router.query
+const PostTitle = ({ title }: Pick<Post, 'title'>) => {
+  return (
+    <>
+      <h1 className="mb-4 text-3xl font-bold tracking-tight text-black md:text-5xl dark:text-white">
+        {title}
+      </h1>
+    </>
+  );
+};
 
+export default function Post({ title, body, author, updated }: Post) {
   return (
     <>
       <Container>
-        <p>slug: {slug}</p>
-        <div className='max-w-screen-md'>
-          <div className="prose" dangerouslySetInnerHTML={{ __html: post.body }} />
+        <PostTitle title={title} />
+        <div className="flex flex-col items-start justify-between w-full mt-4 md:flex-row md:items-center">
+          <div className="flex items-center">
+            <p className="text-sm text-gray-700 dark:text-gray-300">
+              {`${author.first_name} ${author.last_name}`} / {format(new Date(updated), 'MMMM dd, yyyy')}
+            </p>
+          </div>
+          <p className="mt-2 text-sm text-gray-600 dark:text-gray-400 min-w-32 md:mt-0">
+            {readingTime(body).text} • <span>182,232 views</span>
+          </p>
         </div>
+        <div
+          className="prose lg:prose-xl mt-5"
+          dangerouslySetInnerHTML={{ __html: body }}
+        />
       </Container>
     </>
-  )
+  );
 }
 
 // pages/blog/[slug].ts
@@ -34,8 +47,8 @@ export async function getStaticPaths() {
   if (process.env.SKIP_BUILD_STATIC_GENERATION) {
     return {
       paths: [],
-      fallback: 'blocking',
-    }
+      fallback: 'blocking'
+    };
   }
 
   // Call an external API endpoint to get posts
@@ -45,25 +58,25 @@ export async function getStaticPaths() {
   // In production environments, prerender all pages
   // (slower builds, but faster initial page load)
   const paths = blogPosts.map((post) => ({
-    params: { slug: post.slug },
-  }))
+    params: { slug: post.slug }
+  }));
 
   // { fallback: false } means other routes should 404
-  return { paths, fallback: false }
+  return { paths, fallback: false };
 }
 
 export async function getStaticProps({ params }) {
   const butterToken = process.env.NEXT_PUBLIC_BUTTER_CMS_API_KEY;
-  
+
   if (butterToken) {
     try {
-      const blogPost = (await getPost(params.slug));
+      const blogPost: Post = await getPost(params.slug);
 
-      return { props: { post: blogPost } };
+      return { props: { ...blogPost } };
     } catch (e) {
-      throw new Error("Could not get posts!");
+      throw new Error('Could not get posts!');
     }
   }
 
-  return { props: { posts: [], projects: [] } };
+  return { props: { post: {} } };
 }
