532402ef6244b52ca5052f085c9a748262c26b1d
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-12-09 02:14:10 +0100

    add google-analytics

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import Script from 'next/script';
+
 import { Suspense } from 'react';
 
 import Link from 'next/link';
@@ -8,42 +10,55 @@ import Container from '@/ui/Container';
 
 import { projects } from '@/lib/projects';
 
-const HAS_BLOG_POSTS = false
+const HAS_BLOG_POSTS = false;
 
 export default function HomePage() {
   return (
     <Container>
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-Y35CQ4X2FX" />
+      <Script id="google-analytics">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+ 
+          gtag('config', 'G-Y35CQ4X2FX');
+        `}
+      </Script>
+
       <div className="space-y-20">
         <div className="space-y-8">
           <div className="mt-12 max-w-screen-lg space-y-4">
             <Info />
           </div>
           <div className="mt-22 max-w-screen-lg space-y-4">
-            {HAS_BLOG_POSTS && <div className="flex flex-row items-end justify-between mb-5">
-              <h1 className="md:text-2xl font-semibold text-orange-500">
-                Recent Blog Posts
-              </h1>
-              <Link
-                className="flex items-center hover:underline mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
-                href="/blog"
-              >
-                Read all posts
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  className="h-6 w-6 ml-1"
+            {HAS_BLOG_POSTS && (
+              <div className="flex flex-row items-end justify-between mb-5">
+                <h1 className="md:text-2xl font-semibold text-orange-500">
+                  Recent Blog Posts
+                </h1>
+                <Link
+                  className="flex items-center hover:underline mt-8 text-gray-600 dark:text-gray-400 leading-7 rounded-lg hover:text-gray-800 dark:hover:text-gray-200 transition-all h-6"
+                  href="/blog"
                 >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
-                  ></path>
-                </svg>
-              </Link>
-            </div>}
+                  Read all posts
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    className="h-6 w-6 ml-1"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M17.5 12h-15m11.667-4l3.333 4-3.333-4zm3.333 4l-3.333 4 3.333-4z"
+                    ></path>
+                  </svg>
+                </Link>
+              </div>
+            )}
           </div>
 
           <div className="mt-64 max-w-screen-md space-y-4">
@@ -52,9 +67,12 @@ export default function HomePage() {
             </h1>
             <Suspense fallback={<div>loading...</div>}>
               <div className="flex-col space-y-4">
-                {projects.map((project) => (
-                  project.featured && <ProjectLink key={project.id} {...project} />
-                ))}
+                {projects.map(
+                  (project) =>
+                    project.featured && (
+                      <ProjectLink key={project.id} {...project} />
+                    )
+                )}
                 {!projects.length && <div>No Project found.</div>}
               </div>
             </Suspense>
