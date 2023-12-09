281562e2c510f89dd352ca1d6034dff0c7e2b74e
Mahdi Khashan <58775404+mahdikhashan@users.noreply.github.com>
2023-12-09 13:42:31 +0100

    filter and map mdx posts to blog page

--- pages/api/getMdxFiles.ts
+++ pages/api/getMdxFiles.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from 'next';
 
 import fs from 'fs';
 import path from 'path';
+import format from 'date-fns/format';
 
 export default async function handler(
   req: NextApiRequest,
@@ -10,7 +11,23 @@ export default async function handler(
   const blogPath = path.join(process.cwd(), 'pages', 'blog');
   const mdxFiles = fs
     .readdirSync(blogPath)
-    .filter((file) => path.extname(file) === '.mdx')
-    .map((file) => path.basename(file))
-  res.json({ mdxFiles });
+    .filter((file) => path.extname(file) === '.mdx');
+
+  const filesWithDate = mdxFiles
+    .map((file) => {
+      const filePath = path.join(blogPath, file);
+      const stat = fs.statSync(filePath);
+      const formattedDate = format(stat.mtime, 'MMMM dd yyyy');
+      const baseFilename = path.basename(file, path.extname(file));
+      return { file: baseFilename, date: formattedDate };
+    })
+
+  const transformedLinks = filesWithDate.map(link => ({
+      ...link,
+      file: undefined,
+      link: link.file,
+      title: link.file.replace(/-/g, ' ').replace(/\b\w/, match => match.toUpperCase())
+    }));
+  
+  res.json({ mdxFiles: transformedLinks });
 }
